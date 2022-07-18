523f336cc312ae13c1069b3f2ab4782c6da9f336
mjuhbvfr <109461885+mjuhbvfr@users.noreply.github.com>
2022-07-18 21:36:28 +0530

    Update employee.slice.js

--- src/store/employee.slice.js
+++ src/store/employee.slice.js
@@ -43,42 +43,6 @@ export const addEmployeeToList = (employee) => async (dispatch) => {
   dispatch(updateEmployeeList(employee));
 };
 
-// export const getPaymentMethods = () => async (dispatch) => {
-//   const res = await getAvailablePaymentMethods(process.env.REACT_APP_HOTEL_ID);
-//   dispatch(paymentMethods([res.data, res.status]));
-// };
-
-// export const initiatePayment = (data, bookingReference) => async (dispatch) => {
-//   const res = await initiateHoldAmoutPayment(process.env.REACT_APP_HOTEL_ID, bookingReference, data);
-//   dispatch(payments([res.data, res.status]));
-// };
-
-// export const submitAdditionalDetails = (data, orderRef) => async (dispatch) => {
-//   const response = await fetch(`http://localhost:8080/api/submitAdditionalDetails?orderRef=${orderRef}`, {
-//     method: "POST",
-//     body: JSON.stringify(data),
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-//   dispatch(paymentDetails([await response.json(), response.status]));
-// };
-
-// export const getPaymentDataStore = () => async (dispatch) => {
-//   const response = await fetch("http://localhost:8080/api/getPaymentDataStore");
-//   dispatch(paymentDataStore([await response.json(), response.status]));
-// };
-
-// export const cancelOrRefundPayment = (orderRef) => async (dispatch) => {
-//   await fetch(`http://localhost:8080/api/cancelOrRefundPayment?orderRef=${orderRef}`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-//   dispatch(getPaymentDataStore());
-// };
-
 export const employeeList = state => state.organization.employeeList;
 
 export default employeeSlice.reducer;
